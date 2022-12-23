2063f465a1148215e00ca1e07371c9f4fcb910ed
Puri Virakarin <puri.prv@gmail.com>
2022-12-23 20:26:26 +0700

    third commit

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return <nav className="nav">
         <a href="/" className="site-title">Site Name</a>
         <ul>
-            <li>
+            <li className="active">
                 <a href="/pricing">Pricing</a>
             </li>
             <li>

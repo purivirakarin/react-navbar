c1f222cd5c315f3f02a45f305cee555ef903f788
Puri Virakarin <puri.prv@gmail.com>
2022-12-23 23:20:44 +0700

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
-
+import Pricing from './pages/Pricing'
+import About from './pages/About'
+import Home from './pages/Home'
+import { Route , Routes} from "react-router-dom"
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,13 +1,25 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
     return <nav className="nav">
-        <a href="/" className="site-title">Site Name</a>
+        <Link to="/" className="site-title">Site Name</Link>
         <ul>
-            <li className="active">
-                <a href="/pricing">Pricing</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
+            <CustomLink to="/pricing">Pricing</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
         </ul>
     </nav>
+}
+
+interface CustomLinkProps {
+    to: string;
+    children: string;
+}
+
+function CustomLink({ to , children , ...props }: CustomLinkProps) {
+    const path = window.location.pathname 
+    return (
+        <li className={path === to ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
 }
